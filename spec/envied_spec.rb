6b9fe881ad015dc2367943edf325e3496bff8662
Gert Goet <gert@thinkcreate.nl>
2014-03-07 18:29:07 +0100

    Specify
    Run suite via 'bundle exec rspec'

---
+++ spec/envied_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Envied do
+  it { should be }
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require 'rubygems'
+require 'bundler'
+Bundler.setup
+
+$:.unshift File.expand_path("../../lib", __FILE__)
+require 'envied'
