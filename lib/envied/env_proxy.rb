b11511e95589e8cc7c3643bb43195ddf5ec8a98b
Javier Julio <jjfutbol@gmail.com>
2019-04-01 13:56:13 -0400

    Only make necessary methods public
    Many of these are not intended for use outside the proxy. Now its clearer what we rely on as a public interface.

--- lib/envied/env_proxy.rb
+++ lib/envied/env_proxy.rb
@@ -17,14 +17,6 @@ class ENVied
       variables.reject(&method(:coerced?)).reject(&method(:coercible?))
     end
 
-    def variables
-      @variables ||= config.variables.select {|v| groups.include?(v.group) }
-    end
-
-    def variables_by_name
-      @variables_by_name ||= variables.map {|v| [v.name, v] }.to_h
-    end
-
     def [](name)
       coerce(variables_by_name[name.to_sym])
     end
@@ -33,18 +25,12 @@ class ENVied
       variables_by_name[name.to_sym]
     end
 
-    def env_value_of(var)
-      ENV[var.name.to_s]
-    end
-
-    def default_value_of(var)
-      var.default_value(ENVied, var)
-    end
-
     def value_to_coerce(var)
       return env_value_of(var) unless env_value_of(var).nil?
       config.defaults_enabled? ? default_value_of(var) : nil
     end
+
+    private
 
     def coerce(var)
       coerced?(var) ?
@@ -52,16 +38,32 @@ class ENVied
         coercer.coerce(value_to_coerce(var), var.type)
     end
 
+    def coerced?(var)
+      coercer.coerced?(value_to_coerce(var))
+    end
+
     def coercible?(var)
       coercer.coercible?(value_to_coerce(var), var.type)
+    end
+
+    def default_value_of(var)
+      var.default_value(ENVied, var)
+    end
+
+    def env_value_of(var)
+      ENV[var.name.to_s]
     end
 
     def missing?(var)
       value_to_coerce(var).nil?
     end
 
-    def coerced?(var)
-      coercer.coerced?(value_to_coerce(var))
+    def variables
+      @variables ||= config.variables.select {|v| groups.include?(v.group) }
+    end
+
+    def variables_by_name
+      @variables_by_name ||= variables.map {|v| [v.name, v] }.to_h
     end
   end
 end
