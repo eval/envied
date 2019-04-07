cb53acaa4c7d4145ce53cb92b3ba6eaeb87d24ea
Javier Julio <jjfutbol@gmail.com>
2019-04-07 10:18:23 -0400

    Mark class methods as private
    These methods are not meant to be publically accessible and some wouldn't be able to be called directly anyway due to state.

--- lib/envied.rb
+++ lib/envied.rb
@@ -13,6 +13,49 @@ class ENVied
     def required?
       !env.nil?
     end
+
+    private
+
+    def env!(requested_groups, **options)
+      @config = options.fetch(:config) { Configuration.load }
+      @env = EnvProxy.new(@config, groups: required_groups(*requested_groups))
+    end
+
+    def required_groups(*groups)
+      splitter = ->(group){ group.is_a?(String) ? group.split(/ *, */) : group }
+      result = groups.compact.map(&splitter).flatten
+      result.any? ? result.map(&:to_sym) : [:default]
+    end
+
+    def error_on_missing_variables!(**options)
+      names = env.missing_variables.map(&:name)
+      if names.any?
+        msg = "The following environment variables should be set: #{names.join(', ')}."
+        msg << "\nPlease make sure to stop Spring before retrying." if spring_enabled? && !options[:via_spring]
+        raise msg
+      end
+    end
+
+    def error_on_uncoercible_variables!(**options)
+      errors = env.uncoercible_variables.map do |v|
+        format("%{name} with %{value} (%{type})", name: v.name, value: env.value_to_coerce(v).inspect, type: v.type)
+      end
+      if errors.any?
+        msg = "The following environment variables are not coercible: #{errors.join(", ")}."
+        msg << "\nPlease make sure to stop Spring before retrying." if spring_enabled? && !options[:via_spring]
+        raise msg
+      end
+    end
+
+    def ensure_spring_after_fork_require(args, **options)
+      if spring_enabled? && !options[:via_spring]
+        Spring.after_fork { ENVied.require(args, options.merge(via_spring: true)) }
+      end
+    end
+
+    def spring_enabled?
+      defined?(Spring) && Spring.respond_to?(:watcher)
+    end
   end
 
   def self.require(*args, **options)
@@ -22,43 +65,6 @@ class ENVied
     error_on_uncoercible_variables!(options)
 
     ensure_spring_after_fork_require(args, options)
-  end
-
-  def self.env!(requested_groups, **options)
-    @config = options.fetch(:config) { Configuration.load }
-    @env = EnvProxy.new(@config, groups: required_groups(*requested_groups))
-  end
-
-  def self.error_on_missing_variables!(**options)
-    names = env.missing_variables.map(&:name)
-    if names.any?
-      msg = "The following environment variables should be set: #{names.join(', ')}."
-      msg << "\nPlease make sure to stop Spring before retrying." if spring_enabled? && !options[:via_spring]
-      raise msg
-    end
-  end
-
-  def self.error_on_uncoercible_variables!(**options)
-    errors = env.uncoercible_variables.map do |v|
-      format("%{name} with %{value} (%{type})", name: v.name, value: env.value_to_coerce(v).inspect, type: v.type)
-    end
-    if errors.any?
-      msg = "The following environment variables are not coercible: #{errors.join(", ")}."
-      msg << "\nPlease make sure to stop Spring before retrying." if spring_enabled? && !options[:via_spring]
-      raise msg
-    end
-  end
-
-  def self.required_groups(*groups)
-    splitter = ->(group){ group.is_a?(String) ? group.split(/ *, */) : group }
-    result = groups.compact.map(&splitter).flatten
-    result.any? ? result.map(&:to_sym) : [:default]
-  end
-
-  def self.ensure_spring_after_fork_require(args, **options)
-    if spring_enabled? && !options[:via_spring]
-      Spring.after_fork { ENVied.require(args, options.merge(via_spring: true)) }
-    end
   end
 
   def self.springify(&block)
@@ -73,10 +79,6 @@ class ENVied
     else
       block.call
     end
-  end
-
-  def self.spring_enabled?
-    defined?(Spring) && Spring.respond_to?(:watcher)
   end
 
   def self.method_missing(method, *args, &block)
